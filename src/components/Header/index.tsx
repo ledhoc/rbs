a87415d6c32505f5e6fc0375508dd554c2cd8ad2
quynh.hoang <quynh.hoang@thebank.vn>
2024-07-26 14:30:14 +0700

    hero section for new design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -41,10 +41,10 @@ const Header = () => {
   return (
     <>
       <header
-        className={`header left-0 top-0 z-40 flex w-full items-center ${
+        className={`header left-0 top-0 z-40 flex h-[85px] w-full items-center ${
           sticky
-            ? "fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition dark:bg-gray-dark dark:shadow-sticky-dark"
-            : "absolute bg-transparent"
+            ? "fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition dark:shadow-sticky-dark"
+            : "absolute border-b-[1px] border-b-primary bg-white"
         }`}
       >
         <div className="container">
--- src/components/Hero/Background.tsx
+++ src/components/Hero/Background.tsx
@@ -7,47 +7,27 @@ const HeroBackground = (props) => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_437_141)">
-      <path
-        d="M920 624C1439.43 649.675 1837.33 606.667 1972 572L2044 -612L-120 -636L-134 672C-3.33337 622.667 394 598 920 624Z"
-        fill="#FFEDED"
-      />
-      <circle
-        cx="117"
-        cy="45"
-        r="289"
-        fill="url(#paint0_linear_437_141)"
-        fill-opacity="0.18"
-      />
-      <circle
-        cx="1991"
-        cy="455"
-        r="126"
-        fill="url(#paint1_linear_437_141)"
-        fill-opacity="0.18"
-      />
-      <path
-        opacity="0.3"
-        d="M-11.5 582.5C143.5 617.5 512.5 647 804 628C1166.47 604.374 1476 582.5 1932 650"
-        stroke="#FF3131"
-        stroke-linejoin="round"
-        stroke-dasharray="11 11"
-      />
-    </g>
+    <path
+      d="M920 624C1439.43 649.675 1837.33 606.667 1972 572L2044 -612L-120 -636L-134 672C-3.33337 622.667 394 598 920 624Z"
+      fill="#FFFBFB"
+    />
+    <circle
+      cx="1991"
+      cy="455"
+      r="126"
+      fill="url(#paint0_linear_2002_1304)"
+      fill-opacity="0.18"
+    />
+    <path
+      opacity="0.3"
+      d="M-11.5 582.5C143.5 617.5 512.5 647 804 628C1166.47 604.374 1476 582.5 1932 650"
+      stroke="#B20000"
+      stroke-linejoin="round"
+      stroke-dasharray="11 11"
+    />
     <defs>
       <linearGradient
-        id="paint0_linear_437_141"
-        x1="-8.00001"
-        y1="-74"
-        x2="208"
-        y2="276"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop stop-color="#FF3131" />
-        <stop offset="1" stop-color="#FFC3C3" />
-      </linearGradient>
-      <linearGradient
-        id="paint1_linear_437_141"
+        id="paint0_linear_2002_1304"
         x1="1936.5"
         y1="403.118"
         x2="2030.67"
@@ -57,9 +37,6 @@ const HeroBackground = (props) => (
         <stop stop-color="#FF3131" />
         <stop offset="1" stop-color="#FFC3C3" />
       </linearGradient>
-      <clipPath id="clip0_437_141">
-        <rect width="1920" height="661" fill="white" />
-      </clipPath>
     </defs>
   </svg>
 );
