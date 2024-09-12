d0504210143bb4216b551709bc75a5556ace9dc5
Quynh Hoang Van <quynh.hoang@thebank.vn>
2024-09-12 11:50:44 +0700

    update profile CV

--- src/components/Profile/CurriculumVitae.tsx
+++ src/components/Profile/CurriculumVitae.tsx
@@ -1,7 +1,47 @@
+import Dot from "@/components/Icons/Dot";
 import Image from "next/image";
 
 export default function CurriculumVitae({ profile }) {
-  const section = () => {};
+  const section = (text: string) => {
+    return (
+      <div className="my-2 flex flex-row">
+        <div className="h-[24px] w-[3px] bg-primary" />
+        <p className="ml-3 text-xl font-semibold text-primary">
+          {text.toUpperCase()}
+        </p>
+      </div>
+    );
+  };
+
+  const dotLine = (isDot?: boolean) => {
+    if (isDot) {
+      return <Dot color={"#D3D3D3"} />;
+    }
+    return (
+      <svg
+        width="9"
+        height="58"
+        viewBox="0 0 9 58"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="4.5" cy="4" r="4" fill="#D3D3D3" />
+        <rect x="3.5" y="10.75" width="2" height="47" fill="#D3D3D3" />
+      </svg>
+    );
+  };
+
+  const timeline = () => {
+    return (
+      <div className="flex flex-row">
+        <div>{dotLine()}</div>
+        <div>
+          
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="relative z-10 overflow-visible">
       <div>
@@ -19,8 +59,13 @@ export default function CurriculumVitae({ profile }) {
       </h3>
       <p className="text-center text-sm text-black">{profile?.position}</p>
       <div className="max-h-[380px] overflow-scroll px-10">
-        <div className="border-[1px] border-[#F0F0F0] p-3">
-          <p className="mb-6 text-gray-light">{profile?.about}</p>
+        <div className="mt-5 rounded-2xl border-[1px] border-[#F0F0F0] p-3">
+          {section("ABOUT")}
+          <p className="mb-2 text-sm text-gray-light">{profile?.about}</p>
+        </div>
+        <div className="mt-5 rounded-2xl border-[1px] border-[#F0F0F0] p-3">
+          {section("Experience")}
+          {timeline()}
         </div>
       </div>
     </div>
--- src/components/Profile/ProfileData.ts
+++ src/components/Profile/ProfileData.ts
@@ -5,6 +5,20 @@ const profiles = {
     position: "CEO/Cofounder",
     about:
       "As an expert on building teams, I am passionate about matching the strong teams of IT engineers with different requirements of each of project. At Red Breed Tech, I can realize my passion and enthusiasm to support customers who have demands to build great projects on developers with high level - high skills. Save efforts of customers and achieve highest efficiency of projects are my working style. Tan Tran",
+    timeLines: [
+      [
+        {
+          id: 1,
+          company: "Red Breed Technology Company",
+          fromTo: "Since 2024",
+        },
+        {
+          id: 2,
+          company: "Red Breed Technology Company",
+          fromTo: "Since 2024",
+        },
+      ],
+    ],
   },
   director: {
     avatar: "/images/whoweare/avatar-director.png",
